9f6c726e929f4f63b3ffc728d683aa6fac2f8086
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-07-18 14:39:46 +0700

    [ken/rey] Add mux router in main, add docker-compose environment

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+	"os"
 )
 
 func multiply(x,y int) int {
 	return x*y
 }
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(os.Getenv("POSTGRES_DB")))
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
