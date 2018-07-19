ad82567ea44ecf0ffbf1f53c15401ed58cc242bf
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-07-19 18:14:55 +0700

    [ken/rey] add method to find service by name

--- services/service_repository.go
+++ services/service_repository.go
@@ -9,6 +9,7 @@ import (
 const getAllServicesQuery = "SELECT id,name,created_at FROM services"
 const createServiceQuery = "INSERT INTO services(name) VALUES ($1)"
 const deleteServiceQuery = "DELETE FROM services WHERE name = $1"
+const findOneServiceByNameQuery = "SELECT id, name, created_at FROM services WHERE name = $1"
 
 func getAll() []service {
 	var services []service
@@ -43,4 +44,12 @@ func deleteService(servicestore service) error {
 	db := context.GetDB()
 	_, err := db.Query(deleteServiceQuery, servicestore.Name)
 	return err
+}
+
+func FindOneServiceByName(name string) (service, error) {
+	service := service{}
+	db := context.GetDB()
+	row := db.QueryRow(findOneServiceByNameQuery, name)
+	err := row.Scan(&service.Id, &service.Name, &service.CreatedAt)
+	return service, err
 }
