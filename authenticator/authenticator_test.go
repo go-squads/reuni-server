472dd7c200fb29b6f2c97402a3b1867eac25871f
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-08-02 16:35:45 +0700

    Add new test for authenticator

--- authenticator/authenticator_test.go
+++ authenticator/authenticator_test.go
@@ -1,1 +1,13 @@
 package authenticator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetJWTHeader(t *testing.T) {
+	json := "{\"alg\":\"RS256\",\"typ\":\"JWT\"}"
+	fetchedJSON := createJWTHeader()
+	assert.Equal(t, json, string(fetchedJSON))
+}
