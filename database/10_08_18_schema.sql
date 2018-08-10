2d36d00c72c249486e2988c4311242543f3379d3
rifki.adrian <rifki.adrian@aux.go-jek.com>
2018-08-10 15:13:20 +0700

    add authorizator middleware to router

--- database/10_08_18_schema.sql
+++ database/10_08_18_schema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 10.4
 -- Dumped by pg_dump version 10.4
 
--- Started on 2018-08-10 03:50:35 UTC
+-- Started on 2018-08-10 07:56:34 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,7 +18,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 12278)
+-- TOC entry 1 (class 3079 OID 13001)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -26,7 +26,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2224 (class 0 OID 0)
+-- TOC entry 2948 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -39,12 +39,12 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 196 (class 1259 OID 16456)
+-- TOC entry 196 (class 1259 OID 16564)
 -- Name: configurations; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
 CREATE TABLE public.configurations (
-    id integer NOT NULL,
+    id serial NOT NULL,
     namespace text,
     version integer DEFAULT 1,
     config_store jsonb,
@@ -58,7 +58,7 @@ CREATE TABLE public.configurations (
 ALTER TABLE public.configurations OWNER TO postgresdev;
 
 --
--- TOC entry 197 (class 1259 OID 16463)
+-- TOC entry 197 (class 1259 OID 16573)
 -- Name: configurations_id_seq; Type: SEQUENCE; Schema: public; Owner: postgresdev
 --
 
@@ -74,7 +74,7 @@ CREATE SEQUENCE public.configurations_id_seq
 ALTER TABLE public.configurations_id_seq OWNER TO postgresdev;
 
 --
--- TOC entry 2225 (class 0 OID 0)
+-- TOC entry 2949 (class 0 OID 0)
 -- Dependencies: 197
 -- Name: configurations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgresdev
 --
@@ -83,7 +83,7 @@ ALTER SEQUENCE public.configurations_id_seq OWNED BY public.configurations.id;
 
 
 --
--- TOC entry 202 (class 1259 OID 16493)
+-- TOC entry 198 (class 1259 OID 16575)
 -- Name: namespaces; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
@@ -91,14 +91,14 @@ CREATE TABLE public.namespaces (
     service_id integer NOT NULL,
     namespace text NOT NULL,
     active_version integer DEFAULT 1,
-    id integer NOT NULL
+    id serial NOT NULL
 );
 
 
 ALTER TABLE public.namespaces OWNER TO postgresdev;
 
 --
--- TOC entry 203 (class 1259 OID 16505)
+-- TOC entry 199 (class 1259 OID 16582)
 -- Name: namespaces_id_seq; Type: SEQUENCE; Schema: public; Owner: postgresdev
 --
 
@@ -114,8 +114,8 @@ CREATE SEQUENCE public.namespaces_id_seq
 ALTER TABLE public.namespaces_id_seq OWNER TO postgresdev;
 
 --
--- TOC entry 2226 (class 0 OID 0)
--- Dependencies: 203
+-- TOC entry 2950 (class 0 OID 0)
+-- Dependencies: 199
 -- Name: namespaces_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgresdev
 --
 
@@ -123,12 +123,12 @@ ALTER SEQUENCE public.namespaces_id_seq OWNED BY public.namespaces.id;
 
 
 --
--- TOC entry 205 (class 1259 OID 32837)
+-- TOC entry 200 (class 1259 OID 16584)
 -- Name: organization; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
 CREATE TABLE public.organization (
-    id integer NOT NULL,
+    id serial NOT NULL,
     name text,
     created_at timestamp without time zone DEFAULT now(),
     updated_at timestamp without time zone DEFAULT now()
@@ -138,7 +138,7 @@ CREATE TABLE public.organization (
 ALTER TABLE public.organization OWNER TO postgresdev;
 
 --
--- TOC entry 204 (class 1259 OID 32835)
+-- TOC entry 201 (class 1259 OID 16592)
 -- Name: organization_id_seq; Type: SEQUENCE; Schema: public; Owner: postgresdev
 --
 
@@ -154,8 +154,8 @@ CREATE SEQUENCE public.organization_id_seq
 ALTER TABLE public.organization_id_seq OWNER TO postgresdev;
 
 --
--- TOC entry 2227 (class 0 OID 0)
--- Dependencies: 204
+-- TOC entry 2951 (class 0 OID 0)
+-- Dependencies: 201
 -- Name: organization_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgresdev
 --
 
@@ -163,7 +163,7 @@ ALTER SEQUENCE public.organization_id_seq OWNED BY public.organization.id;
 
 
 --
--- TOC entry 206 (class 1259 OID 32850)
+-- TOC entry 202 (class 1259 OID 16594)
 -- Name: organization_member; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
@@ -180,13 +180,13 @@ CREATE TABLE public.organization_member (
 ALTER TABLE public.organization_member OWNER TO postgresdev;
 
 --
--- TOC entry 198 (class 1259 OID 16465)
+-- TOC entry 203 (class 1259 OID 16603)
 -- Name: services; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
 CREATE TABLE public.services (
-    id integer NOT NULL,
-    owner_id integer,
+    id serial NOT NULL,
+    organization_id integer,
     name text NOT NULL,
     created_at timestamp(6) without time zone DEFAULT now() NOT NULL,
     updated_at timestamp without time zone DEFAULT now() NOT NULL,
@@ -197,7 +197,7 @@ CREATE TABLE public.services (
 ALTER TABLE public.services OWNER TO postgresdev;
 
 --
--- TOC entry 199 (class 1259 OID 16472)
+-- TOC entry 204 (class 1259 OID 16611)
 -- Name: services_id_seq; Type: SEQUENCE; Schema: public; Owner: postgresdev
 --
 
@@ -213,8 +213,8 @@ CREATE SEQUENCE public.services_id_seq
 ALTER TABLE public.services_id_seq OWNER TO postgresdev;
 
 --
--- TOC entry 2228 (class 0 OID 0)
--- Dependencies: 199
+-- TOC entry 2952 (class 0 OID 0)
+-- Dependencies: 204
 -- Name: services_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgresdev
 --
 
@@ -222,7 +222,7 @@ ALTER SEQUENCE public.services_id_seq OWNED BY public.services.id;
 
 
 --
--- TOC entry 200 (class 1259 OID 16474)
+-- TOC entry 205 (class 1259 OID 16613)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgresdev
 --
 
@@ -240,7 +240,7 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgresdev;
 
 --
--- TOC entry 201 (class 1259 OID 16480)
+-- TOC entry 206 (class 1259 OID 16619)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgresdev
 --
 
@@ -256,8 +256,8 @@ CREATE SEQUENCE public.users_id_seq
 ALTER TABLE public.users_id_seq OWNER TO postgresdev;
 
 --
--- TOC entry 2229 (class 0 OID 0)
--- Dependencies: 201
+-- TOC entry 2953 (class 0 OID 0)
+-- Dependencies: 206
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgresdev
 --
 
@@ -265,7 +265,7 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 2058 (class 2604 OID 16482)
+-- TOC entry 2784 (class 2604 OID 16621)
 -- Name: configurations id; Type: DEFAULT; Schema: public; Owner: postgresdev
 --
 
@@ -273,7 +273,7 @@ ALTER TABLE ONLY public.configurations ALTER COLUMN id SET DEFAULT nextval('publ
 
 
 --
--- TOC entry 2066 (class 2604 OID 16507)
+-- TOC entry 2786 (class 2604 OID 16622)
 -- Name: namespaces id; Type: DEFAULT; Schema: public; Owner: postgresdev
 --
 
@@ -281,7 +281,7 @@ ALTER TABLE ONLY public.namespaces ALTER COLUMN id SET DEFAULT nextval('public.n
 
 
 --
--- TOC entry 2068 (class 2604 OID 32840)
+-- TOC entry 2789 (class 2604 OID 16623)
 -- Name: organization id; Type: DEFAULT; Schema: public; Owner: postgresdev
 --
 
@@ -289,7 +289,7 @@ ALTER TABLE ONLY public.organization ALTER COLUMN id SET DEFAULT nextval('public
 
 
 --
--- TOC entry 2063 (class 2604 OID 16483)
+-- TOC entry 2795 (class 2604 OID 16624)
 -- Name: services id; Type: DEFAULT; Schema: public; Owner: postgresdev
 --
 
@@ -297,7 +297,7 @@ ALTER TABLE ONLY public.services ALTER COLUMN id SET DEFAULT nextval('public.ser
 
 
 --
--- TOC entry 2065 (class 2604 OID 16484)
+-- TOC entry 2796 (class 2604 OID 16625)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgresdev
 --
 
@@ -305,7 +305,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 2077 (class 2606 OID 16502)
+-- TOC entry 2808 (class 2606 OID 16627)
 -- Name: services auth_unique; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -314,7 +314,7 @@ ALTER TABLE ONLY public.services
 
 
 --
--- TOC entry 2075 (class 2606 OID 16486)
+-- TOC entry 2798 (class 2606 OID 16629)
 -- Name: configurations configurations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -323,7 +323,7 @@ ALTER TABLE ONLY public.configurations
 
 
 --
--- TOC entry 2083 (class 2606 OID 24646)
+-- TOC entry 2814 (class 2606 OID 16631)
 -- Name: users email_unique; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -332,7 +332,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2089 (class 2606 OID 16515)
+-- TOC entry 2800 (class 2606 OID 16633)
 -- Name: namespaces id; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -341,7 +341,7 @@ ALTER TABLE ONLY public.namespaces
 
 
 --
--- TOC entry 2091 (class 2606 OID 32847)
+-- TOC entry 2802 (class 2606 OID 16635)
 -- Name: organization name_unique; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -350,7 +350,7 @@ ALTER TABLE ONLY public.organization
 
 
 --
--- TOC entry 2095 (class 2606 OID 32857)
+-- TOC entry 2806 (class 2606 OID 16637)
 -- Name: organization_member organization_member_pkey; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -359,7 +359,7 @@ ALTER TABLE ONLY public.organization_member
 
 
 --
--- TOC entry 2093 (class 2606 OID 32845)
+-- TOC entry 2804 (class 2606 OID 16639)
 -- Name: organization organization_pkey; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -368,7 +368,7 @@ ALTER TABLE ONLY public.organization
 
 
 --
--- TOC entry 2079 (class 2606 OID 16488)
+-- TOC entry 2810 (class 2606 OID 16641)
 -- Name: services services_pkey; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -377,7 +377,7 @@ ALTER TABLE ONLY public.services
 
 
 --
--- TOC entry 2081 (class 2606 OID 16490)
+-- TOC entry 2812 (class 2606 OID 16643)
 -- Name: services unique_name; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -386,7 +386,7 @@ ALTER TABLE ONLY public.services
 
 
 --
--- TOC entry 2085 (class 2606 OID 24644)
+-- TOC entry 2816 (class 2606 OID 16645)
 -- Name: users username_unique; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -395,7 +395,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 2087 (class 2606 OID 16492)
+-- TOC entry 2818 (class 2606 OID 16647)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgresdev
 --
 
@@ -403,7 +403,16 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
--- Completed on 2018-08-10 03:50:40 UTC
+--
+-- TOC entry 2819 (class 2606 OID 24756)
+-- Name: services organization_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgresdev
+--
+
+ALTER TABLE ONLY public.services
+    ADD CONSTRAINT organization_id_fkey FOREIGN KEY (organization_id) REFERENCES public.organization(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+-- Completed on 2018-08-10 07:56:38 UTC
 
 --
 -- PostgreSQL database dump complete
--- server/middleware.go
+++ server/middleware.go
@@ -2,9 +2,14 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/go-squads/reuni-server/authorizator"
+	"github.com/go-squads/reuni-server/helper"
 
 	"github.com/go-squads/reuni-server/appcontext"
 
@@ -34,6 +39,28 @@ func withAuthenticator(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
+	}
+}
+
+func withAuthorizator(next http.HandlerFunc, permission rune) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		auth := authorizator.New(appcontext.GetHelper())
+		serviceName := mux.Vars(r)["service_name"]
+		service, err := services.FindOneServiceByName(serviceName)
+		if err != nil {
+			response.ResponseError("AuthorizatorMiddleware", "", w, helper.NewHttpError(http.StatusNotFound, "Not Found"))
+			return
+		}
+		uid, err := strconv.ParseInt(fmt.Sprintf("%v", r.Context().Value("userId")), 10, 64)
+		if err != nil {
+			response.ResponseError("AuthorizationMiddleware", "", w, helper.NewHttpError(http.StatusInternalServerError, "Cannot parse userId"))
+		}
+		res := auth.Authorize(int(uid), service.Id, permission)
+		if res {
+			next.ServeHTTP(w, r)
+		} else {
+			response.ResponseError("AuthorizationMiddleware", "", w, helper.NewHttpError(http.StatusForbidden, "Forbidden"))
+		}
 	}
 }
 
